8927ce6956fd69333b7f53f677b6e5e2f25c17ce
Lee <lhc1@rice.edu>
2018-01-04 13:32:53 -0600

    added timing ability

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import configurations as conf
 import build_HSPInt_graph as buildintgraph
 import defineBordersFromGraph
 import os
+import time
 
 
 
@@ -10,17 +11,31 @@ def main():
 	blastdir=conf.blastdir
 	blastInfoFilenames=os.listdir(blastdir)
 	#go through every file entered in the "InputFiles/BLASTp_AllToAll_Data" directory
+	#it will mark all the files with '.done' after it is completed
+	#this is to ensure that you can restart the experiment and not have to redo the test you have done already
 	for i, blastInfoFilename in enumerate(blastInfoFilenames):
-		print "processing",blastInfoFilename,"(", int(100*float(i)/float(len(blastInfoFilenames))),"% )"
-		graphFile=buildintgraph.main(blastInfoFilename)
-		defineBordersFromGraph.main(graphFile)
+		if ".done" not in blastInfoFilename:
+			print "processing",blastInfoFilename,"(", int(100*float(i)/float(len(blastInfoFilenames))),"% )"
+			#start timer
+			startTime=time.time()
+			#define the graphs
+			graphFile=buildintgraph.main(blastInfoFilename)
+			#define the borders from the graphs
+			defineBordersFromGraph.main(graphFile)
+			#stop the timer
+			stopTime=time.time()
+			timeDiff=stopTime-startTime
 
-		blastInfoPath=os.path.join(blastdir ,blastInfoFilename)
-		os.rename(blastInfoPath,blastInfoPath+".done")
+			print "completed in ",timeDiff, "seconds"
 
 
-	f=open("TASKCOMPLETED.txt","w")
-	f.close()
+			blastInfoPath=os.path.join(blastdir ,blastInfoFilename)
+			os.rename(blastInfoPath,blastInfoPath+".done")
+
+
+
+	#f=open("TASKCOMPLETED.txt","w")
+	#f.close()
 
 
 if __name__ == '__main__':
